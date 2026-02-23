e6b952ac588e279e13d8c9667d39b4abbf6fb2af
onkar-Nanavare <onkar@corazonkare.com>
2026-02-23 18:08:26 +0530

    upload new file

--- src/upload_model.py
+++ src/upload_model.py
@@ -3,24 +3,19 @@ import os
 from pathlib import Path
 from huggingface_hub import HfApi
 
-# Check if model exists
 model_file = Path("model.pkl")
 if not model_file.exists():
-    raise FileNotFoundError(f"{model_file} not found. Train the model first!")
+    raise FileNotFoundError(f"{model_file} not found!")
 
-# Get Hugging Face token from environment
 hf_token = os.environ.get("HUGGINGFACE_TOKEN")
 if not hf_token:
-    raise ValueError("HUGGINGFACE_TOKEN not set in environment variables!")
+    raise ValueError("HUGGINGFACE_TOKEN not set!")
 
-# Initialize Hugging Face API
 api = HfApi()
-
-# Upload the model
 api.upload_file(
     path_or_fileobj=model_file,
     path_in_repo="model.pkl",
-    repo_id="Onkar000007/spam-mlops-model", 
+    repo_id="Onkar000007/spam-mlops-model",
     repo_type="model",
     token=hf_token
 )
